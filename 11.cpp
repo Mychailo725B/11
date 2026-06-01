c0ecae3fb81a4df1e6eb34531e23631d63cfd81a
Bieliaiev725B <Mykhailo.Bieliaiev@infiz.khpi.edu.ua>
2026-06-01 14:27:04 +0300

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    float a, b;
+
+    cout << "Введіть дійсне значення a: ";
+    cin >> a;
+
+    cout << "Введіть дійсне значення b: ";
+    cin >> b;
+
+    float* pa = &a;
+    float* pb = &b;
+
+    cout << "\n Початкові значення " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    *pa = *pa * 3;
+
+    cout << "\n Після збільшення 'a' в 3 рази " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    float tp = *pa;
+    *pa = *pb;
+    *pb = tp;
+
+    cout << "\n Після обміну місцями " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
