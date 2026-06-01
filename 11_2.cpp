a01b189a32f7c86f8d078dc8c3601be2a796fdfb
Bieliaiev725B <Mykhailo.Bieliaiev@infiz.khpi.edu.ua>
2026-06-01 21:52:21 +0300

    Add files via upload

---
+++ 11_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char* ptr1 = new char;
+    char* ptr2 = new char;
+    char* ptr3 = new char;
+
+    cout << "Введіть перший символ: ";
+    cin >> *ptr1;
+
+    cout << "Введіть другий символ: ";
+    cin >> *ptr2;
+
+    cout << "Введіть третій символ: ";
+    cin >> *ptr3;
+
+    cout << "\nВведені символи: " << *ptr1 << ", " << *ptr2 << ", " << *ptr3 << endl;
+
+    delete ptr1;
+    delete ptr2;
+    delete ptr3;
+
+    return 0;
+}
